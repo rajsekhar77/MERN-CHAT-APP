5df295fd8fd756aefa31d9c01101db3b645bec84
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-06-24 15:28:04 +0530

    last commit

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
