004f10278ae725ff53df936b41a79cf95d36e179
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-06-24 13:28:41 +0530

    last commit

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,12 @@ import userRoutes from "./routes/userroutes.js";
 import connectToMongoDB from "./DB/connecToMongo.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); // to parse the incoming cookies from req.cookies
