0ffeda97b10b3934d60e1556705594a083724932
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-08-19 00:33:08 +0530

    Edited the home page

--- Fronted/src/pages/home/Home.jsx
+++ Fronted/src/pages/home/Home.jsx
@@ -4,9 +4,9 @@ import MessageContainer from '../../components/messages/MessageContainer.jsx';
 
 function Home() {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-      <Siderbar />
-      <MessageContainer />
+    <div className="flex h-[75vh] md:h-[70vh] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <Siderbar />
+        <MessageContainer />
     </div>
   );
 }
